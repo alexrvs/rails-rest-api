7333a0f79af7bcfab3e3ab5f62313c2ae3924a4f
alexrvs <vasyutin4@gmail.com>
2018-03-06 13:00:17 +0200

    JWT authentication - db save

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :products
       devise_for :users
+
+      post 'auth_user' => 'authentication#authenticate_user'
+
     end
   end
 
